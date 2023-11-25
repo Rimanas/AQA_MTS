e0b5a7d8c98a36d983bac5bdd5c913c703451aeb
Alina Rimanas <alina.rimanas@gmail.com>
2023-11-26 00:32:37 +0300

    initial state

--- TypesAndOperatorsHomeWork/Program.cs
+++ TypesAndOperatorsHomeWork/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace FerstApp
+{
+    static class Programm
+    {
+        static void Main(string[] args)
+        { }
+    }
+}

4113e89d66179a5be315102cb816999b16e79909
Alina Rimanas <alina.rimanas@gmail.com>
2023-11-28 18:16:41 +0300

    first simple view of the console calculator without checking for null

--- TypesAndOperatorsHomeWork/Program.cs
+++ TypesAndOperatorsHomeWork/Program.cs
@@ -1,8 +1,56 @@
-﻿namespace FerstApp
+﻿using System;
+using System.Globalization;
+
+namespace FerstApp
 {
-    static class Programm
+    class Programm
     {
         static void Main(string[] args)
-        { }
+        {
+           // Задание 1. Простой консольны калькулятор
+
+                int result;
+            Console.WriteLine("Добро пожаловать в простой консольный калькулятор, использующий действия + , -, :, *");
+
+            Console.WriteLine("Введите первое число:");
+                int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите знак операции  + , -, : или * :");
+            string operation = Console.ReadLine();
+
+            Console.WriteLine("Введите второе число:");
+                int secondNumber = Convert.ToInt32(Console.ReadLine());
+                
+
+                switch (operation)
+                {
+                    case "+":
+                        result = firstNumber + secondNumber;
+                        
+                        Console.WriteLine("Результат операции:  {0}+{1}={2}", firstNumber, secondNumber, result);
+                        break;
+                    case "-":
+                        result = firstNumber - secondNumber;
+                        Console.WriteLine("Результат операции: {0}-{1}={2}", firstNumber, secondNumber, result);
+                        break;
+                    case "*":
+                        result = firstNumber * secondNumber;
+                        Console.WriteLine("Результат операции: {0}*{1}={2}", firstNumber, secondNumber, result);
+                        break;
+                    case "/":
+                        result = firstNumber / secondNumber;
+                        Console.WriteLine("Результат операции: {0}/{1}={2}", firstNumber, secondNumber, result);
+                        break;
+                    default:
+                        Console.WriteLine("Недопустимое значение");
+                        break;
+                }
+                Console.ReadLine();
+                Console.Write("Do you want to continue(y/n):");
+                
+        }
     }
+
+
+
 }

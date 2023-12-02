ac035ef1993d3aab15ae4e0cbb946cb1dc605e45
Alina Rimanas <alina.rimanas@gmail.com>
2023-12-02 13:22:52 +0300

    classwork add classes Client Individual Corporate

---
+++ Polymorphism/Individual.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism;
+public class Individual : Client
+{
+    public string Firstname;
+    public string Lastname;
+
+    public Individual(int id) : base(id)
+    {
+        Console.WriteLine("Individual Base HashCode: " + base.GetHashCode());
+    }
+
+    public override void SendNotification()
+    {
+        base.SendNotification();
+        Console.WriteLine("Уважаемый клиент...");
+    }
+}

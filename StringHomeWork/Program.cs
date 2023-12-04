83a08273b0fc9274d0626619513d834635119237
Alina Rimanas <alina.rimanas@gmail.com>
2023-12-05 00:28:35 +0300

    initial state

---
+++ StringHomeWork/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace StringHomeWork
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            // =================    1 ЗАДАНИЕ  ===========
+
+            Console.WriteLine("Hello Word");
+        }
+    }
+}

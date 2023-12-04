14e748f389b5b2172202223b02ce26e48cc320d6
Alina Rimanas <alina.rimanas@gmail.com>
2023-12-05 01:24:59 +0300

    solution to 1 task

--- StringHomeWork/Program.cs
+++ StringHomeWork/Program.cs
@@ -1,12 +1,26 @@
-﻿namespace StringHomeWork
+﻿using System.Text.RegularExpressions;
+
+namespace StringHomeWork
 {
     class Programm
     {
         static void Main(string[] args)
         {
-            // =================    1 ЗАДАНИЕ  ===========
-
-            Console.WriteLine("Hello Word");
+            // ===== 1 ЗАДАНИЕ. Заменить в строке 'test' на 'testing'. Удалить из текста все символы-цифры  ======
+            Console.WriteLine();
+            Console.WriteLine("1 ЗАДАНИЕ. Заменить в строке 'test' на 'testing'. Удалить из текста все символы-цифры");
+            string originalText = "Test1234 is an11 important compo77nent90. Test 0911requires test11 da1ta. Test009 data is data for99 9000test!00";
+            Console.WriteLine();
+            Console.WriteLine("Оригинальный текст");
+            Console.WriteLine(originalText);
+            Console.WriteLine("Текст с заменой 'test' на 'testing'");
+            originalText = originalText.ToLower();
+            string replaceText = originalText.Replace("test", "testing");
+            Console.WriteLine(replaceText);
+            Console.WriteLine();  
+            Console.WriteLine("Текст без цифр:");
+            string withoutNumText = Regex.Replace(replaceText, "[0-9]", "");
+            Console.WriteLine(withoutNumText);
         }
     }
 }

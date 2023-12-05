675d89823ec723c59e674339213757b4769e0435
Alina Rimanas <alina.rimanas@gmail.com>
2023-12-05 19:06:56 +0300

    classwork 1 part

--- AbstractClasses/Program.cs
+++ AbstractClasses/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using AbstractClasses;
+using AbstractClasses.Game;
+using System.Threading;
+
+// AbstractClassTemplate abstractClassTemplate = new AbstractClassTemplate(); - Ошибка 
+
+// Game Implementation
+
+Character[] characters = new Character[3];
+
+characters[0] = new Wizard(1);
+characters[1] = new Warrior(1);
+characters[2] = new Warrior(2);
+
+foreach (var character in characters)
+{
+    character.Move();
+    character.Attack();
+    character.UseAbility();
+}

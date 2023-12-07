654178f4eafcf98f6954bd0b70edc6afb5aad394
Alina Rimanas <alina.rimanas@gmail.com>
2023-12-07 19:02:38 +0300

    ClassWork 1 part 07 12 2023

---
+++ Interfaces/BaseImpl/Device.cs
@@ -0,0 +1,6 @@
+﻿namespace Interfaces.BaseImpl;
+
+public abstract class Device : IDisplayable
+{
+    public abstract void DisplayInformation(string information);
+}
---
+++ Interfaces/BaseImpl/Device2.cs
@@ -0,0 +1,9 @@
+﻿namespace Interfaces.BaseImpl;
+
+public class Device2 : IDisplayable
+{
+    public void DisplayInformation(string information)
+    {
+        Console.WriteLine($"Device2 Display: {information}");
+    }
+}

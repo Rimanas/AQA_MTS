98fb9fc01140c5039985daad1510143228486a24
Alina Rimanas <alina.rimanas@gmail.com>
2023-12-13 22:09:17 +0300

    correction of symbols in comments

--- ClassesClassWork/Models/Person.cs
+++ ClassesClassWork/Models/Person.cs
@@ -2,15 +2,15 @@
 
 public class Person
 {
-    // Р‘Р»РѕРє РїРѕР»РµР№ (РїРµСЂРµРјРµРЅРЅС‹С…)
-    public string name = "Undefined";   // РёРјСЏ
-    public int age;                     // РІРѕР·СЂР°СЃС‚
+    // Блок полей (переменных)
+    public string name = "Undefined";   // имя
+    public int age;                     // возраст
 
-    // Р‘Р»РѕРє РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂР°
+    // Блок конструктора
 
-    // Р‘Р»РѕРє РјРµС‚РѕРґРѕРІ Рё СЃРІРѕР№СЃС‚РІ
+    // Блок методов и свойств
     public void Print()
     {
-        Console.WriteLine($"РРјСЏ: {name}  Р’РѕР·СЂР°СЃС‚: {age}");
+        Console.WriteLine($"Имя: {name}  Возраст: {age}");
     }
 }

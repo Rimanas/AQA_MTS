23927c03d44ab6576a7b96146fba683c54416661
Alina Rimanas <alina.rimanas@gmail.com>
2024-02-07 01:15:56 +0300

    Complete HomeWork

---
+++ NUnitHomeWork/SeverityAttributesList.cs
@@ -0,0 +1,8 @@
+﻿namespace NUnitHomeWork;
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+//будет использоваться в методах, АllowMultiple = false - запрещаем чтобы было несколько значений
+public class SeverityAttribute : PropertyAttribute
+{
+    public SeverityAttribute(SeverityLevel level)
+        : base(level.ToString()) { }
+}
--- NUnitHomeWork/UnitTest1.cs
+++ NUnitHomeWork/UnitTest1.cs
@@ -4,59 +4,76 @@ namespace NUnitHomeWork
     public class Tests
     {
         private int i;
-        [Test, Order(4)]
+        [Test, Order(1)]
         [Description("Checking the division of a number(int) by number(int)")]
-        [Category("Regression")]
+        [Author("Alina Alina", "alina@example.com")]  
+        [Category("Smoke")]
+        [Severity(SeverityLevel.Critical)]
         [TestCaseSource(typeof(TestData), nameof(TestData.DivideCasesInt))]
         public void DivideTestInt(int a, int b, int expectedRes)
         {
             Console.WriteLine($"{this}: {TestContext.CurrentContext.Test.MethodName}:Checking the division of a number(int) by number(int)");
             Console.WriteLine($"{this}: DivideTestInt ...{i++}");
+            Console.WriteLine($"{this}: DivideTestDouble... {TestContext.CurrentContext.Test.Properties.Get("Severity")}");
             int actualRes = Calculator.DivInt(a, b);
             Assert.That(actualRes, Is.EqualTo(expectedRes));
         }
-        [Test, Order(5)]
+        [Test, Order(2)]
+        [Author("Alina Alina", "alina@example.com")]
         [Description("Checking the division of a number(int) by zero")]
-        [Category("Regression")]
+        [Category("Smoke")]
+        [Severity(SeverityLevel.Critical)]
         public void DivideByZeroInt()
         {
-            Console.WriteLine($"{this}: DivideTestInt ...{i++}");
+            Console.WriteLine($"{this}: DivideByZeroInt ...{i++}");
+            Console.WriteLine($"{this}: DivideByZeroInt... {TestContext.CurrentContext.Test.Properties.Get("Severity")}");
             Assert.Throws<DivideByZeroException>(() => Calculator.DivInt(5,0));
         }
-        [Test, Order(6)]
+        [Test, Order(3)]
+        [Author("Alina Alina", "alina@example.com")]
         [Description("Checking the division of a zero(int) by zero")]
         [Category("Smoke")]
+        [Severity(SeverityLevel.Critical)]
         public void DivideByZeroInt0()
         {
-            Console.WriteLine($"{this}: DivideTestInt ...{i++}");
+            Console.WriteLine($"{this}: DivideByZeroInt0 ...{i++}");
+            Console.WriteLine($"{this}: DivideByZeroInt0... {TestContext.CurrentContext.Test.Properties.Get("Severity")}");
             Assert.Throws<DivideByZeroException>(() => Calculator.DivInt(0, 0));
         }
         // DIV double
-        [Test, Order(1)]
+        [Test, Order(4)]
+        [Author("Alina Alina", "alina@example.com")]
         [Description("Checking the division of a number(double) by number(double)")]
         [Category("Regression")]
+        [Severity(SeverityLevel.Critical)]
         [TestCaseSource(typeof(TestData), nameof(TestData.DivideCasesDouble))]
         public void DivideTestDouble(double a, double b, double expectedRes)
         {
-            Console.WriteLine($"{this}: DivideTestInt ...{i++}");
+            Console.WriteLine($"{this}: DivideTestDouble ...{i++}");
+            Console.WriteLine($"{this}: DivideTestDouble... {TestContext.CurrentContext.Test.Properties.Get("Severity")}");
             double actualRes = Calculator.DivDouble(a, b);
             Assert.That(actualRes, Is.EqualTo(expectedRes));
         }
-
-        [Test, Order(2)]
+        [Test, Order(5)]
+        [Author("Alina Alina", "alina@example.com")]
         [Description("Checking the division of a number(double) by zero")]
         [Category("Regression")]
+        [Severity(SeverityLevel.Major)]
         public void DivideByZeroDouble()
         {
-            Console.WriteLine($"{this}: DivideTestInt ...{i++}");
+            Console.WriteLine($"{this}: DivideByZeroDouble ...{i++}");
+            Console.WriteLine($"{this}: DivideByZeroDouble... {TestContext.CurrentContext.Test.Properties.Get("Severity")}");
             Assert.Throws<Exception>(() => Calculator.DivDouble(5.0d, 0.0d));
         }
         [Test, Order(3)]
+        [Author("Alina Alina", "alina@example.com")]
         [Description("Checking the division of a zero(double) by zero")]
         [Category("Regression")]
+        [Severity(SeverityLevel.Minor)]
         public void DivideByZeroDouble0()
         {
-            Console.WriteLine($"{this}: DivideTestInt ...{i++}");
+            Console.WriteLine($"{this}: DivideByZeroDouble0 ...{i++}");
+            Console.WriteLine($"{this}: DivideByZeroDouble0... {TestContext.CurrentContext.Test.Properties.Get("Severity")}");
             Assert.Throws<Exception>(() => Calculator.DivDouble(0.0d, 0.0d));
         }
     }

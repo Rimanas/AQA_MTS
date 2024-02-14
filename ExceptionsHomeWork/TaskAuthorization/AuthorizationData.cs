02f46e043124097f903640e82b794442d23b554d
Alina Rimanas <alina.rimanas@gmail.com>
2024-02-14 23:03:17 +0300

    initial state and start 1 task

---
+++ ExceptionsHomeWork/TaskAuthorization/AuthorizationData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsHomeWork.TaskAuthorization
+{
+    internal class AuthorizationData
+    {
+        static int maxLengthLogin = 20;
+        public static bool CheckInputData(string login, string password, string confirmPassword) 
+        {
+            return true;
+        }
+    }
+}

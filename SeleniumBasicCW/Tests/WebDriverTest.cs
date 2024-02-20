126091dffdd72c1620e7d4f41eada1b1bb6e7088
Alina Rimanas <alina.rimanas@gmail.com>
2024-02-21 01:13:53 +0300

    CW 1 part

---
+++ SeleniumBasicCW/Tests/WebDriverTest.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using SeleniumBasicCW.Core;
+
+namespace NUnitTest.Tests;
+
+[TestFixture]
+public class WebDriverTest
+{
+    [Test]
+    public void SimpleDriverTest()
+    {
+        IWebDriver webDriver = new SimpleDriver().Driver;
+        webDriver.Navigate().GoToUrl("http://onliner.by");
+        webDriver.Quit();   // убивает процесс вебдрайвера , иначе при запуске следующих тестов , процессы залочатся
+    }
+
+    
+}

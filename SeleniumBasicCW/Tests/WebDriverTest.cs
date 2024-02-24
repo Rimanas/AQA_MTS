22c72b796d2b5e7de05603bd39249a1636600ef9
Alina Rimanas <alina.rimanas@gmail.com>
2024-02-24 20:38:05 +0300

    CW start 27 01 2024

--- SeleniumBasicCW/Tests/WebDriverTest.cs
+++ SeleniumBasicCW/Tests/WebDriverTest.cs
@@ -1,4 +1,6 @@
-﻿using OpenQA.Selenium;
+﻿using NUnitTest.Core;
+using OpenQA.Selenium;
+using SeleniumBasic.Core;
 using SeleniumBasicCW.Core;
 
 namespace NUnitTest.Tests;
@@ -10,9 +12,25 @@ public class WebDriverTest
     public void SimpleDriverTest()
     {
         IWebDriver webDriver = new SimpleDriver().Driver;
+        webDriver.Manage().Window.Maximize(); 
         webDriver.Navigate().GoToUrl("http://onliner.by");
         webDriver.Quit();   // убивает процесс вебдрайвера , иначе при запуске следующих тестов , процессы залочатся
     }
+    [Test]
+    public void AdvancedDriverTest()
+    {
+        IWebDriver webDriver = new AdvancedDriver().GetChromeDriver();
+        webDriver.Manage().Window.Maximize();
+        webDriver.Navigate().GoToUrl("http://onliner.by");
+        webDriver.Quit();
+    }
+    [Test]
+    public void FactoryDriverTest()
+    {
+        IWebDriver webDriver = new Browser().Driver!;
+        webDriver.Manage().Window.Maximize();
+        webDriver.Navigate().GoToUrl("http://onliner.by");
+        webDriver.Quit();
+    }
 
-    
 }

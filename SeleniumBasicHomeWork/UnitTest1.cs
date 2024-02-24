8959bf79a5c4004e5e01cddaa0e4f5900220011e
Alina Rimanas <alina.rimanas@gmail.com>
2024-02-24 21:30:12 +0300

    Start Home Work initialization Driver

---
+++ SeleniumBasicHomeWork/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SeleniumBasicHomeWork
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

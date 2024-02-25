2f2be4c0390c62951c486c5b65005c07a3855a06
Alina Rimanas <alina.rimanas@gmail.com>
2024-02-26 01:44:03 +0300

    fix 1 task start 2 task

--- SeleniumBasicHomeWork/Tests/FirstTest.cs
+++ SeleniumBasicHomeWork/Tests/FirstTest.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
+using System;
 
 namespace NUnitTest.Tests;
 
@@ -10,10 +11,10 @@ public class FirstTest : BaseTest
     {
         Driver.Navigate().GoToUrl("https://bymed.top/calc/%D1%81%D0%BA%D1%84-2148");
         Thread.Sleep(2000);
-        //Driver.SwitchTo().Frame(Driver.FindElement(By.XPath("//iframe[@src]")));
+        Driver.SwitchTo().Frame(Driver.FindElement(By.XPath("//iframe[@src]")));
         IWebElement age = Driver.FindElement(By.Id("age"));                         //поле Возраст
-        IWebElement selectWebElementSex = Driver.FindElement(By.Id("sex"));         //поле Пол
-        SelectElement selectSex = new SelectElement(selectWebElementSex);              
+        IWebElement selectWebElement = Driver.FindElement(By.Id("sex"));         //поле Пол
+        SelectElement selectSex = new SelectElement(selectWebElement);              
         IWebElement creatin = Driver.FindElement(By.Id("cr"));                      //поле Креатин
         IWebElement selectWebElementCrSize = Driver.FindElement(By.Id("cr-size"));  //поле Размер Креатина
         SelectElement crSize = new SelectElement(selectWebElementCrSize);
@@ -26,33 +27,37 @@ public class FirstTest : BaseTest
         IWebElement ckdEpi = Driver.FindElement(By.Id("ckd_epi_res"));
         IWebElement cgeRes = Driver.FindElement(By.Id("cge_res"));
         IWebElement schwartzRes = Driver.FindElement(By.Id("schwartz_res"));
-
+        IWebElement mdrdDiagnosis = Driver.FindElement(By.XPath("//div[@id = 'mdrd']//span[@class='diagnosis']"));
+        IWebElement ckdEpiDiagnosis = Driver.FindElement(By.XPath("//div[@id = 'ckd_epi']//span[@class='diagnosis']"));
+        IWebElement schwartzDiagnosis = Driver.FindElement(By.XPath("//div[@id = 'schwartz']//span[@class='diagnosis']"));
+        
         age.SendKeys("20");
-        selectSex.SelectByIndex(2);    //Женский 
+        Thread.Sleep(2000);
+        selectSex.SelectByValue("F");    //Женский 
         Thread.Sleep(2000);
         creatin.SendKeys("0,003");
         crSize.SelectByText("мг/дл");
         Thread.Sleep(2000);
-        selectRace.SelectByIndex(1);  // раса - Другая
+        selectRace.SelectByValue("O");  // раса - Другая
         Thread.Sleep(2000);
         mass.SendKeys("50");
         grow.SendKeys("165");
         calcButton.Click();
+        Thread.Sleep(5000);
 
         Assert.Multiple(() =>
         {
             //MDRD
-            Assert.That(mdrd.Text, Is.EqualTo("55087.44"));
-            
-
+            Assert.That(mdrd.Text, Is.EqualTo("57640.18"));
+            Assert.That(mdrdDiagnosis.Text, Is.EqualTo("Нормальный уровень СКФ (C1)"));
             //CKD-EPI
-            Assert.That(ckdEpi.Text, Is.EqualTo("724.04"));
-
+            Assert.That(ckdEpi.Text, Is.EqualTo("749.92"));
+            Assert.That(ckdEpiDiagnosis.Text, Is.EqualTo("Нормальный уровень СКФ (C1)"));
             //Формула Кокрофта-Голта
-            Assert.That(cgeRes.Text, Is.EqualTo("22627.31"));
-
+            Assert.That(cgeRes.Text, Is.EqualTo("23611.11"));
             //Формула Шварца
             Assert.That(schwartzRes.Text, Is.EqualTo("30485.84"));
+            Assert.That(schwartzDiagnosis.Text, Is.EqualTo("Нормальный уровень СКФ (C1)"));
         });
     }
 }
---
+++ SeleniumBasicHomeWork/Tests/SecondTestLaminate.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace NUnitTest.Tests;
+
+public class SecondTestLaminate : BaseTest
+{
+    [Test]
+    public void ValidateLaminate()
+    {
+        Driver.Navigate().GoToUrl("https://home-ex.ru/calculation/ ");
+        IWebElement lnRoom = Driver.FindElement(By.Id("ln_room_id"));                          // Длина комнаты
+        IWebElement wdRoom = Driver.FindElement(By.Id("wd_room_id"));                          // Ширина комнаты
+        IWebElement lnLam = Driver.FindElement(By.Id("ln_lam_id"));                            // Длина плашки
+        IWebElement wdLam = Driver.FindElement(By.Id("wd_lam_id"));                            // Ширина плашки
+        IWebElement nPacking = Driver.FindElement(By.Id("n_packing"));                         // Число плашек в упаковке
+        IWebElement area = Driver.FindElement(By.Id("area"));                                  // Площадь упаковки
+        IWebElement directionLaminate = Driver.FindElement(By.Id("direction-laminate-id1"));   // Направление укладки
+        IWebElement layingMethodLam = Driver.FindElement(By.Id("laying_method_laminate"));     // Способ укладки
+        IWebElement indentWalls = Driver.FindElement(By.Id("indent_walls_id"));                // Отступ от стен
+        IWebElement calculateButton = Driver.FindElement(By.Id("btn_calculate"));              // Кнопка Рассчитать
+
+
+        Assert.Multiple(() =>
+        {
+
+           
+        });
+    }
+}

675590bf9a45e4362fc004aff1cf9793b4ebe69a
Alina Rimanas <alina.rimanas@gmail.com>
2024-03-04 02:07:41 +0300

    HW done

--- SauceDemo/Tests/CssSelectorTest.cs
+++ SauceDemo/Tests/CssSelectorTest.cs
@@ -14,22 +14,16 @@ public class CssSelectorTest : BaseTest
         Assert.That(Driver.FindElement(By.CssSelector(".login_container")).Displayed);
         // Поиск по нескольким значениям в аттрибуте class
         Assert.That(Driver.FindElement(By.CssSelector(".login_wrapper .login-box")).Displayed);
-
         // Поиск по нескольким значениям в аттрибуте class
         Assert.That(Driver.FindElement(By.CssSelector(".input_error.form_input")).Displayed);
-
         // Поиск по tag name
         Assert.That(Driver.FindElement(By.CssSelector("input")).Displayed);
-
         // Поиск по tag и значению из аттрибута class
         Assert.That(Driver.FindElement(By.CssSelector("div.login_logo")).Displayed);
-
         // Поиск всех элементов с тэгом h1 ----не обязательно
         Assert.That(Driver.FindElements(By.CssSelector("h4")).Count, Is.EqualTo(2));
-
         // Поиск всех элементов с тэгом h4 у которых непосредственный родитель с тэгом div ----не обязательно
         Assert.That(Driver.FindElements(By.CssSelector("div > h4")).Count, Is.EqualTo(2));
-
         // Поиск всех элементов у которых присутствует аттрибут id со значением заканчивающимся на какое-то value
         Assert.That(Driver.FindElements(By.CssSelector("[id$='ials']")).Count, Is.EqualTo(1));
         // Поиск всех элементов у которых присутствует аттрибут id со значением начинающимся на какое-то слово
@@ -38,19 +32,14 @@ public class CssSelectorTest : BaseTest
         Assert.That(Driver.FindElements(By.CssSelector("[id^=l]")).Count, Is.EqualTo(3));
         // Поиск всех элементов у которых присутствует аттрибут autocorrect со значением содержащим слово целиком
         Assert.That(Driver.FindElements(By.CssSelector("[autocorrect~='off']")).Count, Is.EqualTo(2));
-
         // Поиск всех элементов у которых присутствует аттрибут class со значением содержащим какой текст
         Assert.That(Driver.FindElements(By.CssSelector("[class*='wrapper']")).Count, Is.EqualTo(2));
-
         // Поиск всех элементов у которых присутствует аттрибут placeholder со значением
         Assert.That(Driver.FindElement(By.CssSelector("[placeholder='Username']")).Displayed);
-
         // Поиск элемента с тэгом h4 и которым является первым дочерним элементом ----не обязательно
         Assert.That(Driver.FindElements(By.CssSelector("h4:first-child")).Count, Is.EqualTo(2));
-
         // Поиск элемента с тэгом h4 и которым является последним дочерним элементом ----не обязательно
         Assert.That(Driver.FindElements(By.CssSelector("h4:last-child")).Count, Is.EqualTo(1));
-
         // Поиск элемента с тэгом h4 и которым является n-ым дочерним элементом ----не обязательно
         Assert.That(Driver.FindElements(By.CssSelector("h4:nth-child(1)")).Count, Is.EqualTo(2));
     }
---
+++ SauceDemo/Tests/XPathSelectorsTests.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using SauceDemo.Helpers.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SauceDemo.Tests
+{
+    internal class XPathSelectorsTests : BaseTest
+    {
+        [Test]
+        public void XPathSelectors()
+        {
+            Driver.Navigate().GoToUrl(Configurator.AppSettings.URL);
+            // Поиск элемента у которого есть аттрибут class cо значением error-message-container
+            Assert.That(Driver.FindElement(By.XPath("//*[@class= 'error-message-container']")).Displayed);
+            // Поиск элемента у которого текстовое значение содержит равно  
+            Assert.That(Driver.FindElement(By.XPath("//div[text()='error_user']")).Displayed);
+            // Поиск элемента у которого значение аттрибута содержит подстроку
+            Assert.That(Driver.FindElement(By.XPath("//div[contains(@id, 'login_credentials')]")).Displayed);
+            // Поиск элемента у которого текстовое значение содержит подстроку
+            Assert.That(Driver.FindElement(By.XPath("//h4[contains(text(),'Accepted')]")).Displayed);
+            //Использование ancestor - все предки текущего узла
+            Assert.That(Driver.FindElements(By.XPath("//*[@id='login_credentials']/ancestor::div")).Count, Is.EqualTo(5));
+            // Использование child - все дочерние элементы с тэгa от div
+            Assert.That(Driver.FindElement(By.XPath("//*[@class='login-box']//descendant::div")).Displayed);
+            //Использование following - Выбирает всё в документе после закрытия тэга текущего узла
+            Assert.That(Driver.FindElement(By.XPath("//*[@class='login-box']/following::h4")).Displayed);
+            // Поиск родителя у элемента с тэгом h1
+            Assert.That(Driver.FindElement(By.XPath("//*[@class='login-box']//parent::div")).Displayed);
+            //Использование preceding- Выбирает все узлы, которые появляются перед текущим узлом в документе
+            Assert.That(Driver.FindElement(By.XPath("//*[@class='login-box']/preceding::div")).Displayed);
+            // Поиск элемента у которого есть аттрибут class cо значением и аттрибут name со значением
+            Assert.That(Driver.FindElement(By.XPath("//*[@class='submit-button btn_action' and @id='login-button']")).Displayed);
+            }
+        }
+    }

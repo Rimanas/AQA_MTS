fb10e4fd69870dfc77bdeac78f59bbc6031ef87d
Alina Rimanas <alina.rimanas@gmail.com>
2024-03-04 00:07:09 +0300

    baseLocators and CssLocators test done

---
+++ SauceDemo/Tests/BaseTest.cs
@@ -0,0 +1,24 @@
+﻿using SauceDemo.Helpers.Configuration;
+using NUnit.Framework;
+using SauceDemo.Core;
+using OpenQA.Selenium;
+
+namespace SauceDemo.Tests;
+
+public class BaseTest
+{
+    protected IWebDriver Driver { get; set; }
+
+    [SetUp]
+    public void Setup()
+    {
+        Driver = new Browser().Driver!;
+        Driver.Navigate().GoToUrl(Configurator.AppSettings.URL);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Driver.Quit();
+    }
+}
---
+++ SauceDemo/Tests/BasicLocatorsTest.cs
@@ -0,0 +1,30 @@
+﻿using SauceDemo.Helpers.Configuration;
+using SauceDemo.Tests;
+using OpenQA.Selenium;
+namespace SauceDemo.Tests;
+public class BasicLocatorsTest : BaseTest
+{
+    [Test]
+    public void basicLocatorsTest_1()
+    {
+        Driver.Navigate().GoToUrl(Configurator.AppSettings.URL);
+        // Find webElement by ID
+        Assert.That(Driver.FindElement(By.Id("user-name")).Displayed);
+        // Find webElement by Name
+        Assert.That(Driver.FindElement(By.Name("user-name")).Displayed);
+        // Find webElement by TagName
+        Assert.That(Driver.FindElements(By.TagName("input")).Count, Is.EqualTo(3));
+    }
+
+    [Test]
+    public void basicLocatorsTest_2()
+    {
+        Driver.Navigate().GoToUrl(Configurator.AppSettings.URL);
+        // Find webElement by linkText        
+        Assert.That(Driver.FindElements(By.LinkText("https://fonts.gstatic.com/s/i/productlogos/translate/v14/24px.svg")).Count, Is.EqualTo(0));
+        // Find webElement by className
+        Assert.That(Driver.FindElement(By.ClassName("login_container")).Displayed);
+        // Find webElement by PartialLinkText
+        Assert.That(Driver.FindElements(By.PartialLinkText("/productlogos/translate/")).Count, Is.EqualTo(0));
+    }
+}

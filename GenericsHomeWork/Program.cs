1c6e2448bc8127d754b7233e94c8c7a1b8efd862
Alina Rimanas <alina.rimanas@gmail.com>
2024-03-07 01:06:39 +0300

    1 task done

---
+++ GenericsHomeWork/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GenericsHomeWork.TaskOnePoint;
+
+
+Console.WriteLine("Задание 1. Точка на координатной плоскости");
+//Объекты с разными типами данных
+Point<int> intPoint = new Point<int>(5, 5);
+Point<double> doublePoint = new Point<double>(5.01, 5.009);
+Point<float> floatPoint = new Point<float>(1.009f, 2.001f);
+Point<long> longPoint = new Point<long>(-100000000000, -3000000000);
+Point<string> stringPoint = new Point<string>("x", "y");
+intPoint.Print();
+doublePoint.Print(); 
+floatPoint.Print(); 
+longPoint.Print();  
+stringPoint.Print();    
---
+++ GenericsHomeWork/TaskTwoGenericClass/GenericClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsHomeWork.TaskTwoGenericClass
+{
+    internal class GenericClass<T>
+    {
+        private T[] array;
+        int sizeArray;  //размер массива
+        public void Add(T element)
+        {
+            var tmp = new List<T>(array);
+            tmp.Add(element);
+            array = tmp.ToArray();
+        }
+    }
+}

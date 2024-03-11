24a510380f87cc0e47e884c6e197985a2ae0d16f
Alina Rimanas <alina.rimanas@gmail.com>
2024-03-11 23:34:59 +0300

    edit 3 task

---
+++ DelegateHomeWork/Task3/Task3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateHomeWork.Task3
+{
+    public class Task3
+    {
+        MethodOfArraySort methods = new MethodOfArraySort();
+
+        public delegate int[] Methods(int[] array);
+        public void RunMethodOfArraySort()
+        {
+            Methods bubbleSort = methods.BubbleSort;
+            Methods shellSort = methods.ShellSort;  
+            Console.WriteLine($"");
+        }
+}

78e9f6abb69072f0863a45a9b0595aae0350ad47
Alina Rimanas <alina.rimanas@gmail.com>
2024-03-16 18:25:05 +0300

    continue

---
+++ SeleniumAdvancedCW/Core/AdvancedDriver.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumAdvancedCW.Core
+{
+    public class AdvancedDriver
+    {
+        private string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+        public IWebDriver GetChromeDriver()
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("--incognito");   // запукаем в приватном режиме
+            chromeOptions.AddArguments("--disable-gpu");  // отключаем свой графический процессор
+            chromeOptions.AddArguments("--disable-extensions");  //отключить всякие дополнения
+            //chromeOptions.AddArguments("--headless");
+
+            chromeOptions.SetLoggingPreference(LogType.Browser, LogLevel.All);  // включаем логирование
+            chromeOptions.SetLoggingPreference(LogType.Driver, LogLevel.All);   // включаем логирование
+
+            return new ChromeDriver(path + @"/Resources/", chromeOptions);
+        }
+        public IWebDriver GetFirefoxDriver()
+        {
+            var mimeTypes =
+                "image/png,image/gif,image/jpeg,image/pjpeg,application/pdf,text/csv,application/vnd.ms-excel," +
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" +
+                "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+
+            var ffOptions = new FirefoxOptions();
+            var profile = new FirefoxProfile();
+
+            profile.SetPreference("browser.download.folderList", 2);
+            profile.SetPreference("browser.helperApps.neverAsk.saveToDisk", mimeTypes);
+            profile.SetPreference("browser.helperApps.neverAsk.openFile", mimeTypes);
+            ffOptions.Profile = profile;
+
+            ffOptions.SetLoggingPreference(LogType.Browser, LogLevel.All);
+            ffOptions.SetLoggingPreference(LogType.Driver, LogLevel.All);
+
+            return new FirefoxDriver(ffOptions);
+        }
+    }
+}
---
+++ SeleniumAdvancedCW/Tests/BaseTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using SeleniumAdvancedCW.Core;
+using SeleniumAdvancedCW.Utilites.Configuration;
+using OpenQA.Selenium;
+
+namespace SeleniumAdvancedCW.Tests;
+
+public class BaseTest
+{
+    protected IWebDriver Driver { get; set; }
+
+    [SetUp]
+    public void Setup()
+    {
+        Driver = new Browser().Driver!;
+        Driver.Navigate().GoToUrl(Configurator.AppSettings.URL);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Driver.Quit();
+    }
+}
